3c724f065634fcd82760a2c8c79bea5af777e592
xxai.art <xxai.art@gmail.com>
2023-06-01 21:48:01 +0800

    ♨

--- bash.sh
+++ bash.sh
@@ -4,8 +4,11 @@ DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
 set -ex
 
+mkdir -p out
+
 docker run \
   -v $DIR/misc:/app/misc \
   -v $DIR/img:/app/img \
   -v $DIR/export:/app/export \
+  -v $DIR/out:/app/out \
   -it --rm altclip /bin/bash
